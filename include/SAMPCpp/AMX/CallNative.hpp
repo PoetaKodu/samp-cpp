de64114d7bb9c97fd0d381aaa0f4a445a12d03ba
PoetaKodu <rfpawelsyska@gmail.com>
2021-06-20 17:36:27 +0200

    Added `int*` and `float*` types for native evaluation.

--- include/SAMPCpp/AMX/CallNative.hpp
+++ include/SAMPCpp/AMX/CallNative.hpp
@@ -12,12 +12,20 @@ template <typename... TArgs>
 inline cell callNativeOn(AMX* amx_, AMX_NATIVE native, TArgs&&... args_)
 {
 	constexpr int NumNativeArgs = sizeof...(TArgs);
+	using Expander = int[];
 
 	// Array of allocated resources 
 	cell resources[NumNativeArgs];
+
+	// Array of physical addreses 
+	cell* physicalAddresses[NumNativeArgs];
+
 	// Initialize resource to -1 (none)
 	for(int i = 0; i < NumNativeArgs; ++i)
+	{
 		resources[i] = -1;
+		physicalAddresses[i] = nullptr;
+	}
 
 	// Prepare native parameters array:
 	cell params[NumNativeArgs + 1];
@@ -26,16 +34,33 @@ inline cell callNativeOn(AMX* amx_, AMX_NATIVE native, TArgs&&... args_)
 
 	// Evaluate all parameters to `params` array, in order:
 	{
-		int paramIdx	= 1;
-		int resourceIdx	= 0;
+		int paramIdx		= 1;
+		int resourceIdx		= 0;
+		int physicalAddrIdx	= 0;
 
-		using Expander = int[];
 		// Initializer list to expand in order
-		Expander{ ((params[paramIdx++] = evalAmxParam(amx_, std::forward<TArgs>(args_), resources[resourceIdx++])), 0)... };
+		Expander{ ((params[paramIdx++] = evalAmxParam(amx_,
+				std::forward<TArgs>(args_),
+				resources[resourceIdx++],
+				physicalAddresses[physicalAddrIdx++]
+			)), 0)... };
 	}
 
 	// Execute and collect return value
 	cell returnValue = native(amx_, params);
+
+	// Handle physical addresses (used when using pointers)
+	{
+		int paramIdx		= 1;
+		int resourceIdx		= 0;
+		int physicalAddrIdx	= 0;
+		
+		// Initializer list to expand in order
+		Expander{ ((handlePhysicalAddress(
+				std::forward<TArgs>(args_),
+				physicalAddresses[physicalAddrIdx++]
+			)), 0)... };
+	}
 
 	// Free allocated resources:
 	for(int i = 0; i < NumNativeArgs; ++i)
--- include/SAMPCpp/AMX/ParamEvaluation.hpp
+++ include/SAMPCpp/AMX/ParamEvaluation.hpp
@@ -8,34 +8,85 @@ namespace samp_cpp
 {
 
 template <typename T>
-inline cell evalAmxParam(AMX* amx_, T value_, cell& resourceCell_) = delete;
+inline cell evalAmxParam(AMX* amx_, T value_, cell& resourceCell_, cell*& physicalAddress_) = delete;
 
 template <>
-inline cell evalAmxParam<int>(AMX* amx_, int value_, cell& resourceCell_)
+inline cell evalAmxParam<int>(AMX* amx_, int value_, cell& resourceCell_, cell*& physicalAddress_)
 {
 	return value_;
 }
 
 template <>
-inline cell evalAmxParam<float>(AMX* amx_, float value_, cell& resourceCell_)
+inline cell evalAmxParam<int*>(AMX* amx_, int* value_, cell& resourceCell_, cell*& physicalAddress_)
+{
+	amx_Allot(amx_, 1, &resourceCell_, &physicalAddress_);
+	return resourceCell_;
+}
+
+template <>
+inline cell evalAmxParam<float>(AMX* amx_, float value_, cell& resourceCell_, cell*& physicalAddress_)
 {
 	return amx_ftoc(value_);
 }
 
 template <>
-inline cell evalAmxParam<std::string const&>(AMX* amx_, std::string const& value_, cell& resourceCell_)
+inline cell evalAmxParam<float*>(AMX* amx_, float* value_, cell& resourceCell_, cell*& physicalAddress_)
 {
-	cell cellIdx = amx_NewString(amx_, value_.c_str());
-	resourceCell_ = cellIdx;
-	return cellIdx;
+	amx_Allot(amx_, 1, &resourceCell_, &physicalAddress_);
+	return resourceCell_;
 }
 
 template <>
-inline cell evalAmxParam<const char*>(AMX* amx_, const char* value_, cell& resourceCell_)
+inline cell evalAmxParam<std::string const&>(AMX* amx_, std::string const& value_, cell& resourceCell_, cell*& physicalAddress_)
 {
-	cell cellIdx = amx_NewString(amx_, value_);
-	resourceCell_ = cellIdx;
-	return cellIdx;
+	resourceCell_ = amx_NewString(amx_, value_.c_str());
+	return resourceCell_;
+}
+
+template <>
+inline cell evalAmxParam<const char*>(AMX* amx_, const char* value_, cell& resourceCell_, cell*& physicalAddress_)
+{
+	resourceCell_ = amx_NewString(amx_, value_);
+	return resourceCell_;
+}
+
+
+
+
+
+template <typename T>
+inline void handlePhysicalAddress(T value_, cell* physicalAddress_) = delete;
+
+template <>
+inline void handlePhysicalAddress<int>(int value_, cell* physicalAddress_)
+{
+}
+
+template <>
+inline void handlePhysicalAddress<int*>(int* value_, cell* physicalAddress_)
+{
+	*value_ = *physicalAddress_;
+}
+
+template <>
+inline void handlePhysicalAddress<float>(float value_, cell* physicalAddress_)
+{
+}
+
+template <>
+inline void handlePhysicalAddress<float*>(float* value_, cell* physicalAddress_)
+{
+	*value_ = *reinterpret_cast<float*>(physicalAddress_);
+}
+
+template <>
+inline void handlePhysicalAddress<std::string const&>(std::string const& value_, cell* physicalAddress_)
+{
+}
+
+template <>
+inline void handlePhysicalAddress<const char*>(const char* value_, cell* physicalAddress_)
+{
 }
 
 }
